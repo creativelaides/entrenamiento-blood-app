fb7f016deacd5da46449d6c53f0aacc5faccf500
Aprendiz Aplicaciones Internas <aprendizsigti@ises.com.co>
2026-03-20 08:22:28 -0500

    feat: :bug: Estamos solucionado el bug de primeng con cluade

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
@@ -10,10 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync(), // Necesario para los efectos de PrimeNG
-    providePrimeNG({
-       theme: {
-      preset: Aura // Esto carga el estilo automáticamente
-       }
-   })]
+    provideAnimationsAsync(),
+  ]
 };
